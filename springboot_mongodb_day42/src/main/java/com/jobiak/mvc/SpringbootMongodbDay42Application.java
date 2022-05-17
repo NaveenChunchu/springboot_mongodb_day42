8d225cd31f5549680acf48d097a5acb23492e310
naveenchunchu <chunchunaveen2001@gmail.com>
2022-05-17 10:29:21 +0530

    Completed......

---
+++ springboot_mongodb_day42/src/main/java/com/jobiak/mvc/SpringbootMongodbDay42Application.java
@@ -0,0 +1,13 @@
+package com.jobiak.mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMongodbDay42Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMongodbDay42Application.class, args);
+	}
+
+}
